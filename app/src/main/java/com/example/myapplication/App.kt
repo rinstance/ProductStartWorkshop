167e55170b9dc8f9de3136ff63a7816dbe9f27bb
rinatnurmukhametov <nurmukhametovrr@mos-team.ru>
2024-05-06 11:07:04 +0300

    remove notif

--- app/src/main/java/com/example/myapplication/App.kt
+++ app/src/main/java/com/example/myapplication/App.kt
@@ -4,24 +4,12 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
+import androidx.media3.common.util.NotificationUtil.createNotificationChannel
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        createNotificationChannel()
     }
 
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val serviceChannel = NotificationChannel(
-                channelID,
-                notificationChannelName,
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-
-            val notificationManager = getSystemService(NotificationManager::class.java)
-            notificationManager?.createNotificationChannel(serviceChannel)
-        }
-    }
 }
